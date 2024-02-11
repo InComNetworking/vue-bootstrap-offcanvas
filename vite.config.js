8a6d6669924d792883e7df5512ed45f9ecdce1ba
Gor Martsen <Gor.martsen@gmail.com>
2024-02-11 13:54:51 -0500

    build 1.1.x

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
-import commonjs from "rollup-plugin-commonjs";
-import externalGlobals from "rollup-plugin-external-globals";
 import vue from "@vitejs/plugin-vue";
 
 var mapSettings = {
@@ -13,6 +11,16 @@ var mapSettings = {
   },
 };
 
+function camelize(str) {
+  let arr = str.split("-");
+  let capital = arr.map(
+    (item) => item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()
+  );
+  return capital.join("");
+}
+
+let packageExportName = camelize("vue-bootstrap-offcanvas");
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -20,16 +28,20 @@ export default defineConfig({
     alias: mapSettings.alias,
   },
   build: {
-    outDir: mapSettings.outDir,
+    lib: {
+      entry: "src/index.js",
+      name: packageExportName,
+      // the proper extensions will be added
+      fileName: "vue-bootstrap-offcanvas",
+      formats: ['umd']
+    },
     rollupOptions: {
-      plugins: [
-        commonjs(),
-        externalGlobals({
-          vue: "Vue",
-        }),
-      ],
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
       output: {
-        format: "es",
+        // Provide global variables to use in the UMD build
+        // for externalized deps
         globals: {
           vue: "Vue",
         },
